ee04aa23a20c79bc85ae7e67ac64f8262152a398
drRiear <yurii.danylenko@gmail.com>
2017-12-07 21:46:26 +0100

    base logic implementation

---
+++ Assets/_Scripts/LoginView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class LoginView : View {
+    [SerializeField] Button loginButton;
+
+    void Awake()
+    {
+        loginButton.onClick.AddListener(LoginButtonHandler);
+    }
+
+    void LoginButtonHandler()
+    {
+        Debug.Log("Login button pressed");
+    }
+
+    protected override void OnSetup()
+    {
+        Debug.Log("onSetup view: " + name);
+    }
+}
---
+++ Assets/_Scripts/View.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class View : MonoBehaviour
+{
+    public void Setup()
+    {
+        //doit parent logic
+        OnSetup();
+    }
+
+    protected abstract void OnSetup();
+
+    public void Show() { }
+    public void Hide() { }
+}
