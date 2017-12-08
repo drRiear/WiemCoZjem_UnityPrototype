7ef74b5f57bae5cb445fa2d22b14e1c849685507
drRiear <yurii.danylenko@gmail.com>
2017-12-08 21:06:29 +0100

    New Logo View

---
+++ Assets/_Scripts/LogoView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoView : View
+{
+    void Awake()
+    {
+
+    }
+
+    protected override void OnSetup()
+    {
+        Debug.Log("onSetup view: " + name);
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        
+    }
+    public override void Hide()
+    {
+        base.Hide();
+    }
+
+}
--- Assets/_Scripts/MainView.cs
+++ Assets/_Scripts/MainView.cs
@@ -1,27 +1,40 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
 
-public class MainView : View
-{
+public class MainView : View {
+    [SerializeField] Button loginButton;
+    [SerializeField] Button searchButton;
+    
     void Awake()
     {
+        loginButton.onClick.AddListener(LoginButtonHandler);
+        searchButton.onClick.AddListener(SearchButtonHandler);
+    }
 
+    void LoginButtonHandler()
+    {
+        Debug.Log("Login button pressed");
+        ViewSwitcher.Instance.ShowView<MainView>();
+    }
+
+    void SearchButtonHandler()
+    {
+        ViewSwitcher.Instance.ShowView<SearchView>();
     }
 
     protected override void OnSetup()
     {
         Debug.Log("onSetup view: " + name);
     }
-
     public override void Show()
     {
         base.Show();
-        
+
     }
     public override void Hide()
     {
         base.Hide();
     }
-
 }
